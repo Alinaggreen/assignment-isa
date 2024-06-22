710d090b71874cccce7585b2ba54cb7e34781596
Alina Gruenaug <alina.gruenaug@accenture.com>
2024-06-22 14:21:27 +0200

    AppTest updated

--- src/test/java/com/accenture/jive/assignment/isa/AppTest.java
+++ src/test/java/com/accenture/jive/assignment/isa/AppTest.java
@@ -1,34 +1,11 @@
 package com.accenture.jive.assignment.isa;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
-import java.math.BigDecimal;
-import java.sql.Date;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    // Declaring ANSI_RESET so that we can reset the color
-    public static final String ANSI_RESET = "\u001B[0m";
+public class AppTest {
 
-    // Declaring the color
-    // Custom declaration
-    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static void main(String[] args) {
 
-    // Main driver method
-    public static void main(String[] args)
-    {
-        // Printing the text on console prior adding
-        // the desired color
-        System.out.println(ANSI_YELLOW
-                + "This text is yellow"
-                + ANSI_RESET);
     }
 }
