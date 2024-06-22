eb2f014ad239e4a0db3e68e5e725fa1fcb5722ce
Alina Gruenaug <alina.gruenaug@accenture.com>
2024-06-22 13:26:47 +0200

    UserInteraction colors

--- src/main/java/com/accenture/jive/assignment/isa/commando/UserInteraction.java
+++ src/main/java/com/accenture/jive/assignment/isa/commando/UserInteraction.java
@@ -17,13 +17,19 @@ import java.util.Scanner;
 public class UserInteraction {
 
     private final Scanner scanner;
+    private final String RESET = "\u001B[0m";
+    private final String RED = "\u001B[31m";
+    private final String GREEN = "\u001B[32m";
+    private final String CYAN = "\u001B[36m";
+    private final String MAGENTA = "\u001B[35m";
+    private final String YELLOW = "\u001B[33m";
 
     public UserInteraction(Scanner scanner) {
         this.scanner = scanner;
     }
 
     public void listCommandos () {
-        System.out.println("The following commandos can be executed:");
+        System.out.println(CYAN + "The following commandos can be executed:" + RESET);
         System.out.println("help - shows all commandos that can be called");
         System.out.println("import - imports a CSV file to the database");
         System.out.println("delete - deletes all data from the database");
@@ -39,49 +45,49 @@ public class UserInteraction {
     }
 
     public void successfulCommando () {
-        System.out.println("The requested commando was successfully executed.");
+        System.out.println(GREEN + "The requested commando was successfully executed." + RESET);
     }
 
     public void successTermination () {
-        System.out.println("The requested commando has been canceled.");
+        System.out.println(MAGENTA + "The requested commando has been canceled." + RESET);
     }
 
     public String failedCommandoSQL () {
-        return "The requested commando could not be executed due to issues with the database.";
+        return RED + "The requested commando could not be executed due to issues with the database." + RESET;
     }
 
     public String failedCommandoIO () {
-        return "The requested commando could not be executed due to issues with the output operation.";
+        return RED + "The requested commando could not be executed due to issues with the output operation." + RESET;
     }
 
     public String failedCommandoFile () {
-        return "The requested commando could not be executed because the file could not be found. " +
-                "Please try again.";
+        return RED + "The requested commando could not be executed because the file could not be found. " +
+                "Please try again." + RESET;
     }
 
     public String failedCommandoNumberFormat () {
-        return "The requested commando could not be executed because the prices do not conform " +
-                "to the number format. Please try again.";
+        return RED + "The requested commando could not be executed because the prices do not conform " +
+                "to the number format. Please try again." + RESET;
     }
 
     public String failedCommandoDateTime () {
-        return "The requested commando could not be executed because the dates do not conform " +
-                "to the date format. Please try again.";
+        return RED + "The requested commando could not be executed because the dates do not conform " +
+                "to the date format. Please try again." + RESET;
     }
 
     public void missingStock () {
-        System.out.println("There is no stock with this ID in the database. Please try again.");
+        System.out.println(MAGENTA + "There is no stock with this ID in the database. Please try again." + RESET);
     }
 
     public String readImportName () {
-        System.out.println("Please enter the file path of the CSV file you want to import " +
-                "(for example STOCK_DATA.csv):");
+        System.out.println("Please enter the file path of the CSV file you want to import "
+                + YELLOW + "(for example STOCK_DATA.csv)" + RESET + ":");
         return scanner.nextLine();
     }
 
     public String readExportName () {
-        System.out.println("Please enter the file path where you would like to save the new CSV file " +
-                "(for example STOCK_DATA_export.csv):");
+        System.out.println("Please enter the file path where you would like to save the new CSV file "
+                + YELLOW + "(for example STOCK_DATA_export.csv)" + RESET + ":");
         return scanner.nextLine();
     }
 
@@ -91,10 +97,9 @@ public class UserInteraction {
         try {
             return Integer.parseInt(inputId);
         } catch (NumberFormatException e) {
-            System.out.println("Please enter a number!");
-            readCompanyId();
+            System.out.println(RED + "Please enter a number!" + RESET);
+            return readCompanyId();
         }
-        return 0;
     }
 
     public BigDecimal readPrice () {
@@ -103,10 +108,9 @@ public class UserInteraction {
         try {
             return new BigDecimal(inputPrice);
         } catch (NumberFormatException e) {
-            System.out.println("Please enter a number!");
-            readPrice();
+            System.out.println(RED + "Please enter a number!" + RESET);
+            return readPrice();
         }
-        return null;
     }
 
     public LocalDate readDate () {
@@ -115,10 +119,9 @@ public class UserInteraction {
         try {
             return LocalDate.parse(date, DateTimeFormatter.ofPattern("dd.MM.yy"));
         } catch (DateTimeParseException e) {
-            System.out.println("Please enter a date!");
-            readDate();
+            System.out.println(RED + "Please enter a date!" + RESET);
+            return readDate();
         }
-        return null;
     }
 
     public String readIndustry () {
@@ -127,37 +130,42 @@ public class UserInteraction {
     }
 
     public String knowCompany () {
-        System.out.println("Do you know the ID of the company?");
+        System.out.println("Do you know the ID of the company?"  + YELLOW + " yes / no" + RESET);
         return scanner.nextLine();
     }
 
     public String knowIndustry () {
-        System.out.println("Do you know the name of the new industry?");
+        System.out.println("Do you know the name of the new industry?"  + YELLOW + " yes / no" + RESET);
         return scanner.nextLine();
     }
 
     public String shouldAdd () {
-        System.out.println("Are you sure you want to add this stockmarket entry?");
+        System.out.println("Are you sure you want to add this stockmarket entry?"
+                + YELLOW + " yes / no" + RESET);
         return scanner.nextLine();
     }
 
     public String shouldDelete() {
-        System.out.println("Are you sure you want to delete everything from the database?");
+        System.out.println("Are you sure you want to delete everything from the database?"
+                + YELLOW + " yes / no" + RESET);
         return scanner.nextLine();
     }
 
     public String shouldExport () {
-        System.out.println("Are you sure you want to export the data from the database?");
+        System.out.println("Are you sure you want to export the data from the database?"
+                + YELLOW + " yes / no" + RESET);
         return scanner.nextLine();
     }
 
     public String shouldImport () {
-        System.out.println("Are you sure you want to import the data to the database?");
+        System.out.println("Are you sure you want to import the data to the database?"
+                + YELLOW + " yes / no" + RESET);
         return scanner.nextLine();
     }
 
     public String shouldUpdate() {
-        System.out.println("Are you sure you want to update this stock's industry?");
+        System.out.println("Are you sure you want to update this stock's industry?"
+                + YELLOW + " yes / no" + RESET);
         return scanner.nextLine();
     }
 
@@ -173,9 +181,11 @@ public class UserInteraction {
 
     public void printCompanyPlaceholder (List<Stock> stocks, String userCommando) {
         if (stocks.isEmpty()) {
-            System.out.println("There is currently no company starting with " + userCommando + " in the database.");
+            System.out.println(MAGENTA + "There is currently no company starting with " + RESET
+                    + "'" + userCommando + "'" + MAGENTA + " in the database." + RESET);
         } else {
-            System.out.println("The following companies start with " + userCommando + ":");
+            System.out.println(CYAN + "The following companies start with " + RESET
+                    + "'" + userCommando + "'" + CYAN + ":" + RESET);
             for (Stock stock : stocks) {
                 System.out.println("ID: " + stock.getId() + " - " + stock.getName());
             }
@@ -184,9 +194,11 @@ public class UserInteraction {
 
     public void printIndustryPlaceholder (List<Industry> industries, String userCommando) {
         if (industries.isEmpty()) {
-            System.out.println("There is currently no industry starting with " + userCommando + " in the database.");
+            System.out.println(MAGENTA + "There is currently no industry starting with "+ RESET
+                    + "'" + userCommando + "'" + MAGENTA + " in the database." + RESET);
         } else {
-            System.out.println("The following industries start with " + userCommando + ":");
+            System.out.println(CYAN + "The following industries start with " + RESET
+                    + "'" + userCommando + "'" + CYAN + ":" + RESET);
             for (Industry industry : industries) {
                 System.out.println(industry.getName());
             }
@@ -195,9 +207,9 @@ public class UserInteraction {
 
     public void printIndustry (List<Industry> industries) {
         if (industries.isEmpty()) {
-            System.out.println("There are currently no industries in the database.");
+            System.out.println(MAGENTA + "There are currently no industries in the database." + RESET);
         } else {
-            System.out.println("These are all the industries with the number of stocks assigned:");
+            System.out.println(CYAN + "These are all the industries with the number of stocks assigned:"+ RESET);
             for (Industry industry : industries) {
                 System.out.println("ID: " + industry.getId() + " - " + industry.getName()
                         + " - currently " + industry.getStockCount() + " stocks assigned");
@@ -206,36 +218,37 @@ public class UserInteraction {
     }
 
     public void showIndustry (String industry) {
-        System.out.println("The company is currently assigned to the following industry: " + industry);
+        System.out.println("The company is currently assigned to the following industry: " + CYAN + industry + RESET);
     }
 
     public void printPrice (List<Stockmarket> stockmarkets) {
         if (stockmarkets.isEmpty()) {
-            System.out.println("There are currently no stockmarket entries for this stock.");
+            System.out.println(MAGENTA + "There are currently no stockmarket entries for this stock." + RESET);
         } else {
-            System.out.println("These are the last ten prices for this stock:");
+            System.out.println(CYAN + "These are the last ten prices for this stock:" + RESET);
             for (Stockmarket stockmarket : stockmarkets) {
-                System.out.println(stockmarket.getMarketPrice() + " € on " + stockmarket.getMarketDate());
+                System.out.println(stockmarket.getMarketPrice() + "€ on " + stockmarket.getMarketDate());
             }
         }
     }
 
     public boolean foundCompany () {
-        System.out.println("Did you find the desired company ID?");
+        System.out.println("Did you find the desired company ID?" + YELLOW + " yes / no" + RESET);
         String foundCompany = scanner.nextLine();
         return !"yes".equalsIgnoreCase(foundCompany);
     }
 
     public boolean foundIndustry () {
-        System.out.println("Did you find the desired industry?");
+        System.out.println("Did you find the desired industry?" + YELLOW + " yes / no" + RESET);
         String foundCompany = scanner.nextLine();
         return !"yes".equalsIgnoreCase(foundCompany);
     }
 
     public void maxPrice (Stockmarket stockmarket) {
         if (stockmarket.getStockId() != null) {
-            System.out.println("The highest price for this stock was " + stockmarket.getMarketPrice() + "€ on "
-                    + stockmarket.getMarketDate() + ".");
+            System.out.println("The highest price for this stock was " + CYAN
+                    + stockmarket.getMarketPrice() + "€" + RESET + " on " + CYAN
+                    + stockmarket.getMarketDate() + RESET + ".");
         } else {
             noEntries();
         }
@@ -243,8 +256,9 @@ public class UserInteraction {
 
     public void minPrice (Stockmarket stockmarket) {
         if (stockmarket.getStockId() != null) {
-            System.out.println("The lowest price for this stock was " + stockmarket.getMarketPrice() + "€ on "
-                    + stockmarket.getMarketDate() + ".");
+            System.out.println("The lowest price for this stock was " + CYAN
+                    + stockmarket.getMarketPrice() + "€" + RESET + " on " + CYAN
+                    + stockmarket.getMarketDate() + RESET + ".");
         } else {
             noEntries();
         }
@@ -252,21 +266,23 @@ public class UserInteraction {
 
     public void gapPrice (Stockmarket stockmarketMax, Stockmarket stockmarketMin) {
         if (stockmarketMax.getMarketPrice().equals(stockmarketMin.getMarketPrice())) {
-            System.out.println("There is currently only one stockmarket entry for this stock: "
-                    + stockmarketMax.getMarketPrice() + "€ on " + stockmarketMax.getMarketDate());
+            System.out.println("There is currently only one stockmarket entry for this stock: " + CYAN
+                    + stockmarketMax.getMarketPrice() + "€" + RESET + " on " + CYAN +
+                    stockmarketMax.getMarketDate() + RESET + ".");
         } else {
             maxPrice(stockmarketMax);
             minPrice(stockmarketMin);
-            System.out.println("The difference between highest and lowest price is "
-                    + stockmarketMax.getMarketPrice().subtract(stockmarketMin.getMarketPrice()) + "€.");
+            System.out.println("The difference between highest and lowest price is " + CYAN
+                    + stockmarketMax.getMarketPrice().subtract(stockmarketMin.getMarketPrice()) +
+                    "€" + RESET + ".");
         }
     }
 
     public void noEntries () {
-        System.out.println("There are currently no stock market entries for this company.");
+        System.out.println(MAGENTA + "There are currently no stock market entries for this company." + RESET);
     }
 
     public void goodbye() {
-        System.out.println("Thank you very much and Goodbye!");
+        System.out.println(GREEN + "Thank you very much and Goodbye!" + RESET);
     }
 }
